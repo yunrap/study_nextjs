966fed01bd4ab719f791ab1e40da2b302594399d
yunrap <dbsdpwl7@gmail.com>
2024-11-28 10:17:53 +0900

    first init

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/reset.css';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '@/components/common/Layout';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
